6e05e70bf99236f2e649bc246c87370cbd24c69e
Pablo Hernández <17086478+Hadronomy@users.noreply.github.com>
2024-06-18 17:15:33 +0100

    refactor: rename `ui/model` to `ui/program`

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -6,11 +6,11 @@ import (
 
 	tea "github.com/charmbracelet/bubbletea"
 
-	"github.com/hadronomy/midas/internal/ui/model"
+	"github.com/hadronomy/midas/internal/ui/program"
 )
 
 func main() {
-	_, err := tea.NewProgram(model.NewModel()).Run()
+	_, err := tea.NewProgram(program.NewModel()).Run()
 	if err != nil {
 		fmt.Println("Oh no:", err)
 		os.Exit(1)
--- internal/ui/program/program.go
+++ internal/ui/program/program.go
@@ -1,4 +1,4 @@
-package model
+package program
 
 import (
 	"fmt"
